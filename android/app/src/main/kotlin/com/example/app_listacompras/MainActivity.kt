06c038907ac41f41a54f2a4e70e06bde534a9966
Daniel Carneiro <daniel@dmcsysteminformatica.com.br>
2023-08-02 10:23:29 -0300

    Project Created

---
+++ android/app/src/main/kotlin/com/example/app_listacompras/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.app_listacompras
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:app_listacompras/pages/home.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MaterialApp(debugShowCheckedModeBanner: false, home: Home()));
+}
